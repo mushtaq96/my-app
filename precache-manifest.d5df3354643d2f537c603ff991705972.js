acd6fe7cd90b2fd511127fc2bc79fcfd5ad80e64
mushtaq96 <mushtaq96smb@gmail.com>
2020-07-29 18:19:57 +0900

    Updates

---
+++ precache-manifest.d5df3354643d2f537c603ff991705972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19a0ace637201c51b00e4ac7cf6bd01",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "97b9f8d5d55ae73725e4",
+    "url": "/my-app/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "c9a791c33144c12ea1cd",
+    "url": "/my-app/static/js/2.0da06914.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.0da06914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b9f8d5d55ae73725e4",
+    "url": "/my-app/static/js/main.aa4f158a.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  }
+]);
