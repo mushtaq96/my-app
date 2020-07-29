2d1f92fbc1496fe35938a96feeb2efd6e08c8fc3
mushtaq96 <mushtaq96smb@gmail.com>
2020-07-29 16:05:33 +0900

    testing again

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css'
 
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value:null,
-    };
-  }
-  
   render() {
     return (
-      <button className="square" onClick={()=> alert('click')}>
-        {this.props.value}
+      <button className="square">
+        {/* TODO */}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i){
-    return <Square value={i}/>;
+  renderSquare(i) {
+    return <Square />;
   }
 
   render() {
