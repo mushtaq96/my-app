a7abd85cf19586511b875ac7b640883666924421
mushtaq96 <mushtaq96smb@gmail.com>
2020-07-29 18:26:03 +0900

    at this point, child element Square receives values from Board component ie parent

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
 
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button 
+        className="square" 
+        onClick={()=>this.props.onClick()}
+      >  
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice(); //slice method copies and returns a new array object
+    squares[i]='x';
+    this.setState({squares:squares});
+  }
   renderSquare(i) {
-    return <Square />;
+    return(
+      <Square
+      value = {this.state.squares[i]}
+      onClick={()=>this.handleClick(i)}
+      />
+    ); 
   }
 
   render() {
